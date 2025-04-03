1d7593c388297359cb0ab764d7c6ac0187155498
Dhruv Pal <dhruvpal.4818@gmail.com>
2025-04-03 22:19:21 +0530

    update height of elements

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 const navLinks = document.querySelectorAll(".headerCenter a");
-navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        setTimeout(() => {
-            document.querySelector(".headerCenter").classList.remove("active");
-        }, 500);
-    });
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    setTimeout(() => {
+      document.querySelector(".headerCenter").classList.remove("active");
+    }, 500);
+  });
 });
 const hamburger = document.querySelector(".hamburgerMenu");
-hamburger.onclick = function() {
-    document.querySelector(".headerCenter").classList.toggle("active");
+hamburger.onclick = function () {
+  document.querySelector(".headerCenter").classList.toggle("active");
 };
 
 const themeBtn = document.querySelector(".themeBtn");
 function toggleTheme() {
-    themeBtn.classList.toggle("fa-moon");
-    themeBtn.classList.toggle("fa-sun");
-    document.body.classList.toggle("dark-theme");
+  themeBtn.classList.toggle("fa-moon");
+  themeBtn.classList.toggle("fa-sun");
+  document.body.classList.toggle("dark-theme");
 }
 themeBtn.addEventListener("click", toggleTheme);
 
 var typed = new Typed(".auto-type", {
-    strings: ["Frontend Web Developer", "UI/UX Designer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true
+  strings: ["Frontend Developer", "Backend Developer", "UI/UX Designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
 });
 
 var loader = document.getElementById("preloader");
-window.addEventListener("load", function(){
-    loader.style.top = "-200%";
+window.addEventListener("load", function () {
+  loader.style.top = "-200%";
 });
