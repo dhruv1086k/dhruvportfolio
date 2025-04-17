f7378c14b6edcf457159b049b2e58f1875c8d672
Dhruv Pal <dhruvpal.0905@gmail.com>
2025-04-17 23:44:21 +0530

    update new

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fallingStar: "fallingStar 5s linear forwards",
+      },
+      keyframes: {
+        fallingStar: {
+          "0%": { transform: "translate(0, 0) scale(var(--tw-scale-x))" },
+          "100%": {
+            transform: "translate(120vw, 120vh) scale(var(--tw-scale-x))",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
